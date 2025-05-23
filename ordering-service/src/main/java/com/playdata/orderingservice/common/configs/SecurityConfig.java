8bcec056e457e038d978a6bee4de748eab5bd711
hajoon <stravinskky@naver.com>
2025-05-23 09:36:35 +0900

    다시 코어스

--- ordering-service/src/main/java/com/playdata/orderingservice/common/configs/SecurityConfig.java
+++ ordering-service/src/main/java/com/playdata/orderingservice/common/configs/SecurityConfig.java
@@ -31,7 +31,9 @@ public class SecurityConfig {
         // 스프링 시큐리티에서 기본으로 제공하는 CSRF 토큰 공격을 방지하기 위한 장치 해제.
         // CSRF(Cross Site Request Forgery) 사이트 간 요청 위조
         http.csrf(csrf -> csrf.disable());
+        http.cors(Customizer.withDefaults());
 //        http.cors(Customizer.withDefaults()); // 직접 커스텀한 CORS 설정을 적용.
+
 
         // 세션 관리 상태를 사용하지 않고
         // STATELESS한 토큰을 사용하겠다.
