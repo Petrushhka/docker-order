3ec9747d59b48ff92a66e5577a996afb0f82e07d
hajoon <stravinskky@naver.com>
2025-05-23 10:23:38 +0900

    이름 바꾸기

--- ordering-service/src/main/java/com/playdata/orderingservice/common/configs/CorsConfig.java
+++ ordering-service/src/main/java/com/playdata/orderingservice/common/configs/CorsConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @Configuration
-public class WebMvcConfigurer {
+public class CorsConfig {
 
     @Bean
     public org.springframework.web.servlet.config.annotation.WebMvcConfigurer corsConfigurer() {
